aed2faa556ec9cbc2ecb3c4250a7583ae885639b
mdaniyalk <muchammad.daniyal.kautsar@mail.ugm.ac.id>
2023-07-01 13:19:48 +0700

    fix: some error

--- bookstore_api/views_add.py
+++ bookstore_api/views_add.py
@@ -51,7 +51,22 @@ def get_writer_id(name):
         writer = Writer.objects.get(writerName=name)
         return writer.writerId
     except Writer.DoesNotExist:
-        return None     
+        return None   
+
+
+def get_address_id(name):
+    try:
+        address = Address.objects.get(addressName=name)
+        return address.addressId
+    except Address.DoesNotExist:
+        return None  
+
+def get_store_id(name):
+    try:
+        store = Store.objects.get(storeName=name)
+        return store.storeId
+    except Store.DoesNotExist:
+        return None
 
 def get_datetime_from_year(year):
     datetime_value = datetime.datetime(int(year), 1, 1)
@@ -169,6 +184,137 @@ def addEditWriter(request, type, id):
     return False
 
 
+@transaction.atomic
+def addEditStaff(request, type, id):
+    if request.method == 'POST':
+        name = request.POST['name']
+        address_id = request.POST['address_id']
+        phoneNum = request.POST['phoneNum']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        address = Address.objects.get(pk=address_id)
+        
+        if type == 'add':
+            Staff.objects.create(name=name, addressId=address, phoneNum=phoneNum, email=email, username=username, password=password)
+        elif type == 'edit':
+            obj = Staff.objects.get(pk=id)
+            obj.name = name
+            obj.addressId = address
+            obj.phoneNum = phoneNum
+            obj.email = email
+            obj.username = username
+            obj.password = password
+            obj.save()
+        
+        return True
+    return False
+
+@transaction.atomic
+def addEditCustomer(request, type, id):
+    if request.method == 'POST':
+        name = request.POST['name']
+        address_id = request.POST['address_id']
+        phoneNum = request.POST['phoneNum']
+        email = request.POST['email']
+        
+        address = Address.objects.get(pk=address_id)
+        
+        if type == 'add':
+            Customer.objects.create(name=name, addressId=address, phoneNum=phoneNum, email=email)
+        elif type == 'edit':
+            obj = Customer.objects.get(pk=id)
+            obj.name = name
+            obj.addressId = address
+            obj.phoneNum = phoneNum
+            obj.email = email
+            obj.save()
+        
+        return True
+    return False
+
+@transaction.atomic
+def addEditStore(request, type, id):
+    if request.method == 'POST':
+        staff_id = request.POST['staff_id']
+        address_id = request.POST['address_id']
+        
+        staff = Staff.objects.get(pk=staff_id)
+        address = Address.objects.get(pk=address_id)
+        
+        if type == 'add':
+            Store.objects.create(staffId=staff, addressId=address)
+        elif type == 'edit':
+            obj = Store.objects.get(pk=id)
+            obj.staffId = staff
+            obj.addressId = address
+            obj.save()
+        
+        return True
+    return False
+
+@transaction.atomic
+def addEditInventory(request, type, id):
+    if request.method == 'POST':
+        store_id = request.POST['store_id']
+        book_id = request.POST['book_id']
+        
+        store = Store.objects.get(pk=store_id)
+        book = Book.objects.get(pk=book_id)
+        
+        if type == 'add':
+            Inventory.objects.create(storeId=store, bookId=book)
+        elif type == 'edit':
+            obj = Inventory.objects.get(pk=id)
+            obj.storeId = store
+            obj.bookId = book
+            obj.save()
+        
+        return True
+    return False
+
+@transaction.atomic
+def addEditPayment(request, type, id):
+    if request.method == 'POST':
+        method = request.POST['method']
+        value = request.POST['value']
+        staff_id = request.POST['staff_id']
+        
+        staff = Staff.objects.get(pk=staff_id)
+        
+        if type == 'add':
+            Payment.objects.create(method=method, value=value, staffId=staff)
+        elif type == 'edit':
+            obj = Payment.objects.get(pk=id)
+            obj.method = method
+            obj.value = value
+            obj.staffId = staff
+            obj.save()
+        
+        return True
+    return False
+
+@transaction.atomic
+def addEditAddress(request, type, id):
+    if request.method == 'POST':
+        streetName = request.POST['streetName']
+        buildingNum = request.POST['buildingNum']
+        regency = request.POST['regency']
+        state = request.POST['state']
+        
+        if type == 'add':
+            Address.objects.create(streetName=streetName, buildingNum=buildingNum, regency=regency, state=state)
+        elif type == 'edit':
+            obj = Address.objects.get(pk=id)
+            obj.streetName = streetName
+            obj.buildingNum = buildingNum
+            obj.regency = regency
+            obj.state = state
+            obj.save()
+        
+        return True
+    return False
 
 
 
@@ -189,4 +335,8 @@ def addEdit_views_wrapper(token, request, type, id=None):
         response = addEditStore(request, type, id)
     elif token == 'inventory':
         response = addEditInventory(request, type, id)
+    elif token == 'address':
+        response = addEditAddress(request, type, id)
+    elif token == 'payment':
+        response = addEditPayment(request, type, id)
     return response
